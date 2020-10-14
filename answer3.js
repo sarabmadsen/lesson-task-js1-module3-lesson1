8f06880e3b3da02dc6c1373aa82d6eb7005d1acf
Connor O'Brien <connor@hifranc.com>
2020-10-14 18:17:46 +0200

    Update answer3.js

--- answer3.js
+++ answer3.js
@@ -1,13 +1,9 @@
 async function getCatFacts() {
     const url = "https://cat-fact.herokuapp.com/facts";
-    try {
-        const response = await fetch(url);
-        const results = await response.json();
-        const facts = results.all;
-        console.log(facts.length);
-    } catch (err) {
-        console.log(err);
-    }
+    const response = await fetch(url);
+    const results = await response.json();
+    const facts = results.all;
+    console.log(facts.length);
 }
 
 getCatFacts();
