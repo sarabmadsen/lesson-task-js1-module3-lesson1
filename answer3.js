509b42704a124a4dea78c17605bdeea99afe587e
Connor O'Brien <connorbobrien@gmail.com>
2021-02-01 13:27:54 +0400

    Update answer3.js

--- answer3.js
+++ answer3.js
@@ -2,8 +2,7 @@ async function getCatFacts() {
     const url = "https://cat-fact.herokuapp.com/facts";
     const response = await fetch(url);
     const results = await response.json();
-    const facts = results.all;
-    console.log(facts.length);
+    console.log(results.length);
 }
 
 getCatFacts();
